cc6aa4d2ca8a6a03f54de345bf0b2b9e95f79423
Keith <hnefatl@gmail.com>
2023-07-30 00:09:51 +0200

    Remove some hopefully-redundant config values

--- display-deploy/dietpi-wifi.txt.sh
+++ display-deploy/dietpi-wifi.txt.sh
@@ -12,17 +12,6 @@ aWIFI_SSID[0]='${INIT_WIFI_SSID}'
 aWIFI_KEY[0]='${INIT_WIFI_PSK}'
 # - Key type: NONE (no key/open) | WPA-PSK | WEP | WPA-EAP (then use settings below)
 aWIFI_KEYMGR[0]='WPA-PSK'
-# - WPA-EAP options: Only fill if WPA-EAP is set above
-aWIFI_PROTO[0]=''
-aWIFI_PAIRWISE[0]=''
-aWIFI_AUTH_ALG[0]=''
-aWIFI_EAP[0]=''
-aWIFI_IDENTITY[0]=''
-aWIFI_PASSWORD[0]=''
-aWIFI_PHASE1[0]=''
-aWIFI_PHASE2[0]=''
-# - Path to the certificate file, e.g.: /boot/mycert.cer
-aWIFI_CERT[0]=''
 
 # Entry 1
 # - WiFi SSID: required, case sensitive
@@ -33,15 +22,4 @@ aWIFI_SSID[1]='${DEST_WIFI_SSID}'
 aWIFI_KEY[1]='${DEST_WIFI_PSK}'
 # - Key type: NONE (no key/open) | WPA-PSK | WEP | WPA-EAP (then use settings below)
 aWIFI_KEYMGR[1]='WPA-PSK'
-# - WPA-EAP options: Only fill if WPA-EAP is set above
-aWIFI_PROTO[1]=''
-aWIFI_PAIRWISE[1]=''
-aWIFI_AUTH_ALG[1]=''
-aWIFI_EAP[1]=''
-aWIFI_IDENTITY[1]=''
-aWIFI_PASSWORD[1]=''
-aWIFI_PHASE1[1]=''
-aWIFI_PHASE2[1]=''
-# - Path to the certificate file, e.g.: /boot/mycert.cer
-aWIFI_CERT[1]=''
 "
