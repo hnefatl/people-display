6bafacda233db1e08d7bbefecf1d391494a69948
Keith <hnefatl@gmail.com>
2023-08-26 16:15:33 +0200

    Let tag script print most recent tag

--- tag.sh
+++ tag.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]] ; then
-  echo "Usage: tag.sh <tag> <message>"
+if [[ $# -lt 1 ]] ; then
+  echo "Usage: tag.sh (display|exporter) <version>"
   exit 1
 fi
 
-git tag -a "$1" -m "$2"
-git push origin "$1"
+latest_tag=$(git describe --match "$1*" | sed "s/$1-\(.*\)-.*-.*/\1/")
+echo "Latest tag is $latest_tag"
+
+if [[ $# -ne 2 ]] ; then
+  exit 2
+fi
+
+tag="$1-$2"
+echo "This tag is $tag"
+
+git tag -a "$tag" -m ""
+git push origin "$tag"
