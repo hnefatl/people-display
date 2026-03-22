4c200ea26f1763a9a20099fef2a2e83a8498224f
Keith Collister <hnefatl@gmail.com>
2026-03-22 15:04:51 +0000

    Tidy up flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-    flake-parts.url = "github:hercules-ci/flake-parts";
     naersk.url = "github:nix-community/naersk";
     # Just used for rustChannelOf
     nixpkgs-mozilla.url = "github:mozilla/nixpkgs-mozilla";
@@ -15,63 +14,53 @@
       naersk,
       nixpkgs-mozilla,
     }:
-    flake-parts.lib.mkFlake { inherit inputs; } (
-      { ... }:
-      {
-        systems = [ "x86_64-linux" ];
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (import nixpkgs-mozilla)
+        ];
+        config = { };
+      };
 
-        perSystem =
-          {
-            system,
-            config,
-            pkgs,
-            ...
-          }:
-          let
-            toolchain =
-              (pkgs.rustChannelOf {
-                rustToolchain = ./rust-toolchain.toml;
-                sha256 = "sha256-27Fpm3bG8ax9gh0qzwqg1ef3Y/HDX73DplSkJUzmCyc=";
-              }).rust;
+      toolchain =
+        (pkgs.rustChannelOf {
+          rustToolchain = ./rust-toolchain.toml;
+          sha256 = "sha256-27Fpm3bG8ax9gh0qzwqg1ef3Y/HDX73DplSkJUzmCyc=";
+        }).rust;
 
-            naersk' = pkgs.callPackage naersk {
-              cargo = toolchain;
-              rustc = toolchain;
-            };
-          in
-          {
-            _module.args.pkgs = import nixpkgs {
-              inherit system;
-              overlays = [
-                (import nixpkgs-mozilla)
-              ];
-              config = { };
-            };
+      naersk' = pkgs.callPackage naersk {
+        cargo = toolchain;
+        rustc = toolchain;
+      };
 
-            packages.default = naersk'.buildPackage rec {
-              src = ./.;
-              nativeBuildInputs = with pkgs; [
-                autoPatchelfHook
-              ];
-            };
+      # Shared between both the below, so that `nix build` and `cargo build` both work.
+      shared = {
+        nativeBuildInputs = with pkgs; [
+          cargo
+          pkg-config
+          toolchain
+          openssl_3
+          SDL2
+          SDL2_image
+          protobuf
+        ];
+        RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.SDL2
+          pkgs.SDL2_image
+        ];
+      };
+    in
+    {
+      packages.${system}.default = naersk'.buildPackage rec {
+        src = ./.;
+        inherit (shared) nativeBuildInputs RUST_SRC_PATH LD_LIBRARY_PATH;
+      };
 
-            devShells.default = pkgs.mkShell {
-              nativeBuildInputs = with pkgs; [
-                cargo
-                pkg-config
-                toolchain
-                openssl_3
-                SDL2
-                SDL2_image
-                protobuf
-              ];
-              RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
-              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-                pkgs.SDL2
-                pkgs.SDL2_image
-              ];
-            };
-          };
-      }
-    );
+      devShells.${system}.default = pkgs.mkShell {
+        inherit (shared) nativeBuildInputs RUST_SRC_PATH LD_LIBRARY_PATH;
+      };
+    };
 }
