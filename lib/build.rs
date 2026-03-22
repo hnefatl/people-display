19f32e17aca1cba8af6c283906532a28b985c973
Keith Collister <hnefatl@gmail.com>
2026-03-22 16:42:20 +0000

    Cargo upgrades

--- lib/build.rs
+++ lib/build.rs
@@ -1,4 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("lib/clock.proto")?;
+    tonic_prost_build::compile_protos("lib/clock.proto")?;
     Ok(())
 }
